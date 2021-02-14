3d919cba7031029a5d67b7dde73a94174b3f1f1c
Desmond Aldridge <desmondaldridge@gmail.com>
2021-02-14 12:21:11 -0600

    LilHeader component adjustment

--- src/components/LilHeader.js
+++ src/components/LilHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './LilHeader.css';
 
-const LilHeader = ({ text}) => {
+const LilHeader = ({ text , img}) => {
   return (
     <div className={'lil_header'}>
-      <p>{text}</p>
+      <p>{text}</p><img className='icons' src={img} alt=' ' />
     </div>
   );
 };
 
 export default LilHeader;
+
